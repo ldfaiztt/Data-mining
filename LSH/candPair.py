552f25a8e4f8c5cb3f527f4cd332e4d0944c5e1a
Chisheng Li <chisli@shapiro100017.ccs.itd.umich.edu>
2014-12-15 09:16:19 -0500

    file update

--- LSH/candPair.py
+++ LSH/candPair.py
@@ -9,14 +9,14 @@
 #   6   1   6   4   1   1   4
 
 def candPair(x, y):
-  i = 0
-  while i < len(x):
-    j = i + 1
-    while j < len(x):
-      if x[i] == x [j] and y[i] == y[j]:
-        print "C%s,C%s" % (i+1,j+1)
-      j += 1
-    i += 1
+	i = 0
+	while i < len(x):
+		j = i + 1
+		while j < len(x):
+			if x[i] == x [j] and y[i] == y[j]:
+				print "C%s,C%s" % (i+1,j+1)
+			j += 1
+		i += 1
 
 candPair([1,2,1,1,2,5,4], [2,3,4,2,3,2,2])
 candPair([3,1,2,3,1,3,2], [4,1,3,1,2,4,4])
