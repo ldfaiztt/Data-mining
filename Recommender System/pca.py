b944ed7451eba69b8337d6bdbd2665c2be4f698b
Chisheng Li <chisli@trotter1.ccs.itd.umich.edu>
2014-12-15 13:53:31 -0500

    files update

--- Recommender System/pca.py
+++ Recommender System/pca.py
@@ -7,7 +7,23 @@ MTM = np.dot(MT, M)
 
 print "M: "
 print M
+
+#M: 
+#[[1 1]
+# [2 2]
+# [3 4]]
+
+
 print "MT: "
 print MT
+
+#MT: 
+#[[1 2 3]
+# [1 2 4]]
+
 print "MTM: "
 print MTM
+
+#MTM: 
+#[[14 17]
+# [17 21]]
